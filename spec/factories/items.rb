e58c89610886d8aa046508c115a61cb187f328e7
David Atkins <atki1080@gmail.com>
2020-03-16 23:07:56 -0600

    Add item factory

---
+++ spec/factories/items.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :item do
+    name { "A Worldly Possession" }
+    description { "You don't need to need one to buy one" }
+    unit_price { 10.59 }
+    merchant { nil }
+  end
+end
