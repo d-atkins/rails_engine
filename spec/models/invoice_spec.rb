7831a39b61cf2b3e38e5e8f3496f7d0717d1486c
David Atkins <atki1080@gmail.com>
2020-03-16 22:52:56 -0600

    Test enum for status

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Invoice, type: :model do
   describe 'validations' do
     it { should validate_presence_of :status }
+    it { should define_enum_for(:status).with_values([:shipped]) }
   end
 
   describe 'relationships' do
