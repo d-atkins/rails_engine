40788900c10b1040aefff2379e6ee9d76cdeba91
David Atkins <atki1080@gmail.com>
2020-03-16 23:10:54 -0600

    Test enum for result

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -5,6 +5,8 @@ describe Transaction, type: :model do
     it { should validate_presence_of :credit_card_number }
     it { should validate_presence_of :credit_card_expiration_date }
     it { should validate_presence_of :result }
+    it { should define_enum_for(:result).with_values([:failed, :success]) }
+
   end
 
   describe 'relationships' do
