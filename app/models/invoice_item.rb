c003ca23e97e4050ab9e26ea0a1847ac5e9f1b0c
David Atkins <atki1080@gmail.com>
2020-03-18 12:40:16 -0600

    Add before_create for price conversion

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,4 +2,8 @@ class InvoiceItem < ApplicationRecord
   validates_presence_of :quantity, :unit_price
   belongs_to :item
   belongs_to :invoice
+
+  before_create do
+    self.unit_price /= 100.0
+  end
 end
