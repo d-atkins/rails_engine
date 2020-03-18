766cb89adab443f4b933b84717cd956f51319840
David Atkins <atki1080@gmail.com>
2020-03-18 15:10:44 -0600

    Change import to not print to terminal by default

--- lib/helpers/importer.rb
+++ lib/helpers/importer.rb
@@ -6,36 +6,36 @@ class Importer
     @models = [Customer, Merchant, Item, Invoice, InvoiceItem, Transaction]
   end
 
-  def import(path, model)
-    print "Importing #{model} records... "
+  def import(path, model, print)
+    print "Importing #{model} records... " if print
     csv_text = File.read("#{path}#{model.to_s.underscore}s.csv")
     csv = CSV.parse(csv_text, :headers => true)
     csv.each do |row|
       model.create(row.to_hash)
     end
-    puts "DONE"
+    puts "DONE" if print
   end
 
-  def destroy_all_records
+  def destroy_all_records(print)
     @models.reverse.each do |model|
-      print "Destroying #{model} records... "
+      print "Destroying #{model} records... " if print
       model.destroy_all
-      puts "DONE"
+      puts "DONE" if print
     end
   end
 
-  def reset_pk_sequences
+  def reset_pk_sequences(print)
     ActiveRecord::Base.connection.tables.each do |t|
       ActiveRecord::Base.connection.reset_pk_sequence!(t)
     end
-    puts "PK sequences reset."
+    puts "PK sequences reset." if print
   end
 
-  def reset_all_tables(path)
-    destroy_all_records
-    @models.each { |model| import(path, model) }
-    reset_pk_sequences
-    puts "Reseed complete!"
+  def reset_all_tables(path, print = false)
+    destroy_all_records(print)
+    @models.each { |model| import(path, model, print) }
+    reset_pk_sequences(print)
+    puts "Reseed complete!" if print
   end
 
 end
