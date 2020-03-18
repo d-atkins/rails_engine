a1693161db1d118c02aff54a6dfe6f934018a581
David Atkins <atki1080@gmail.com>
2020-03-18 15:12:15 -0600

    Add most_revenue test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,5 +8,20 @@ describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many :invoices }
     it { should have_many :items }
+    it { should have_many(:transactions).through(:invoices) }
+  end
+
+  describe 'methods' do
+    before(:each) do
+      importer = Importer.new
+      importer.reset_all_tables('./spec/fixtures/truncated/')
+    end
+
+    it 'most_revenue' do
+      expected = [Merchant.second, Merchant.first, Merchant.third]
+      expect(Merchant.most_revenue(3)).to eq(expected)
+      expect(Merchant.most_revenue(2)).to eq([expected[0], expected[1]])
+      expect(Merchant.most_revenue(1)).to eq([expected[0]])
+    end
   end
 end
