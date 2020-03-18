9fdb7b6d58b9920ec517b8575e9a2e09c5e7ae3b
David Atkins <atki1080@gmail.com>
2020-03-18 12:40:40 -0600

    Remove price conversion from importer

--- lib/helpers/importer.rb
+++ lib/helpers/importer.rb
@@ -6,16 +6,11 @@ class Importer
     @models = [Customer, Merchant, Item, Invoice, InvoiceItem, Transaction]
   end
 
-  def convert_price(price)
-    price /= 100.0
-  end
-
   def import(model)
     print "Importing #{model} records... "
     csv_text = File.read("./db/csv_seeds/#{model.to_s.underscore}s.csv")
     csv = CSV.parse(csv_text, :headers => true)
     csv.each do |row|
-      row['unit_price'] = convert_price(row['unit_price'].to_f) if row['unit_price']
       model.create(row.to_hash)
     end
     puts "DONE"
