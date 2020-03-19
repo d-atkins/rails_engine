ed2f36442518cc2123f8d0963e3298301c979b41
David Atkins <atki1080@gmail.com>
2020-03-19 17:35:01 -0600

    Limit response to one merchant

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
-    render json: MerchantSerializer.new(Merchant.search(request.query_parameters))
+    merchant = Merchant.search(request.query_parameters).take
+    render json: MerchantSerializer.new(merchant)
   end
 end
