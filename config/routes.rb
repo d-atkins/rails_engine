83a5e38b592ad0bded25cc133975ac53a64d1e5c
David Atkins <atki1080@gmail.com>
2020-03-18 19:20:30 -0600

    Add merchants update route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
+        resources :most_revenue, only: [:index]
         get '/', to: 'merchants#index'
         post '/', to: 'merchants#create'
-        resources :most_revenue, only: [:index]
         get '/:id', to: 'merchants#show'
+        patch '/:id', to: 'merchants#update'
       end
     end
   end
