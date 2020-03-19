a9b830bfd593d384d869e658b1f3ab87ef65d813
David Atkins <atki1080@gmail.com>
2020-03-18 21:52:27 -0600

    Add show action

--- app/controllers/api/v1/items/items_controller.rb
+++ app/controllers/api/v1/items/items_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::Items::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.all)
   end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
 end
