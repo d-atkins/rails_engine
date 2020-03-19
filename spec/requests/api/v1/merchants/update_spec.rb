6633dee4fa675599e7f627bfc102329e32da14dc
David Atkins <atki1080@gmail.com>
2020-03-18 19:16:23 -0600

    Add merchants update spec

---
+++ spec/requests/api/v1/merchants/update_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'Merchants API:' do
+  before(:each) do
+    importer = Importer.new
+    importer.reset_all_tables('./spec/fixtures/truncated/')
+  end
+
+  it 'sends the new info of an updated merchant' do
+    patch '/api/v1/merchants/3', params: {merchant: {name: 'Superb Ghost Shop'}}
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant['data']['id']).to eq('3')
+    expect(merchant['data']['attributes']['name']).to eq('Superb Ghost Shop')
+    expect(Merchant.last.name).to eq('Superb Ghost Shop')
+  end
+
+end
