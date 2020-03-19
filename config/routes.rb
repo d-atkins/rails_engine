4128a424293f4b9525b3026ec9971528f614937d
David Atkins <atki1080@gmail.com>
2020-03-19 14:20:03 -0600

    Add items merchant show route

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
         get '/:id', to: 'items#show'
         patch '/:id', to: 'items#update'
         delete '/:id', to: 'items#destroy'
+        get '/:item_id/merchant', to: 'merchant#show'
       end
     end
   end
