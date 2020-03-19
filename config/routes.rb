c0262affb2f09d367d39e8f5a5935ea329c85a63
David Atkins <atki1080@gmail.com>
2020-03-18 19:10:28 -0600

    Add merchants create route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :merchants do
         get '/', to: 'merchants#index'
+        post '/', to: 'merchants#create'
         resources :most_revenue, only: [:index]
         get '/:id', to: 'merchants#show'
       end
