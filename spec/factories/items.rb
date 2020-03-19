41b7b51b9e923b9f14a63166a063b3900635e7a5
David Atkins <atki1080@gmail.com>
2020-03-18 22:40:06 -0600

    Add associations to factories

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { "A Worldly Possession" }
     description { "You don't need to need one to buy one" }
     unit_price { 10.59 }
-    merchant { nil }
+    association :merchant
   end
 end
