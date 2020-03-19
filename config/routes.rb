2459c9b446fd60488a8f206792c1edaffc9c90e9
David Atkins <atki1080@gmail.com>
2020-03-18 18:53:17 -0600

    Add merchants index route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
+        get '/', to: 'merchants#index'
         resources :most_revenue, only: [:index]
       end
     end
