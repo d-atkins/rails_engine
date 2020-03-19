007c37be0b60f504a124dc84ba37225907a880d1
David Atkins <atki1080@gmail.com>
2020-03-18 19:46:05 -0600

    Add merchants destroy spec

---
+++ spec/requests/api/v1/merchants/destroy_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Merchants API:' do
+  before(:each) do
+    importer = Importer.new
+    importer.reset_all_tables('./spec/fixtures/truncated/')
+  end
+
+  it 'sends the new info of a deleted merchant' do
+    expect(Merchant.count).to eq(3)
+    expect(Merchant.last.name).to eq('Ghost Shop')
+
+    delete '/api/v1/merchants/3'
+
+    deleted_merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(deleted_merchant['data']['id']).to eq('3')
+    expect(deleted_merchant['data']['attributes']['name']).to eq('Ghost Shop')
+    expect(Merchant.last.name).to_not eq('Ghost Shop')
+    expect{Merchant.find(3)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+end
