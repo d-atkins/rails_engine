f13abcb00ea6493235984867da3bd8de5180eb27
David Atkins <atki1080@gmail.com>
2020-03-19 08:43:56 -0600

    Add merchant items index spec

---
+++ spec/requests/api/v1/merchants/items/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Merchants API:', type: :request do
+  before(:each) do
+    importer = Importer.new
+    importer.reset_all_tables('./spec/fixtures/truncated/')
+  end
+
+  it 'sends a list of all merchants' do
+    get '/api/v1/merchants/2/items'
+
+    merchants = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchants['data'].length).to eq(6)
+    expect(merchants['data'].first['id']).to eq('10')
+    expect(merchants['data'].first['type']).to eq('item')
+    expect(merchants['data'].second['id']).to eq('11')
+    expect(merchants['data'].last['id']).to eq('15')
+  end
+
+end
