0a812d73eb76424f9f9041ea5584798dd6eed847
David Atkins <atki1080@gmail.com>
2020-03-18 21:51:06 -0600

    Add items show spec

---
+++ spec/requests/api/v1/items/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Items API:', type: :request do
+  before(:each) do
+    importer = Importer.new
+    importer.reset_all_tables('./spec/fixtures/truncated/')
+  end
+
+  it 'sends one items by id' do
+    get '/api/v1/items/16'
+
+    items = JSON.parse(response.body)
+    last = Item.last
+
+    expect(response).to be_successful
+    expect(items['data']['id']).to eq(16)
+    expect(items['data']['attributes']['name']).to eq(last.name)
+    expect(items['data']['attributes']['description']).to eq(last.description)
+    expect(items['data']['attributes']['unit_price']).to eq(last.unit_price)
+  end
+
+end
