e45cb1643fc7259f9a26f1852a05ff442c2c628e
David Atkins <atki1080@gmail.com>
2020-03-19 18:03:45 -0600

    Add search and partial_search tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,5 +34,26 @@ describe Item, type: :model do
     it 'partial_matchables' do
       expect(Item.partial_matchables).to eq(['name', 'description'])
     end
+
+    it 'search' do
+      search_params = {'unit_price' => '751.07', 'merchant_id' => '1'}
+
+      expect(Item.search(search_params)).to eq([Item.first])
+
+      expected = [3, 5, 7, 8, 12]
+      search_params = {'name' => 'Item E'}
+
+      expect(Item.search(search_params).pluck(:id)).to eq(expected)
+    end
+
+    it 'partial_search' do
+      search_params = {'name' => 'tem mi'}
+
+      expect(Item.search(search_params)).to eq([Item.second])
+
+      search_params = {'description' => 'lupTATe Aut Lab'}
+
+      expect(Item.search(search_params)).to eq([Item.fifth])
+    end
   end
 end
