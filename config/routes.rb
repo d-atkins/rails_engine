3f0d09c1a775718092b5573b30f1dbb80f1446e9
David Atkins <atki1080@gmail.com>
2020-03-19 21:33:26 -0600

    Add merchants search index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :merchants do
         resources :most_revenue, only: [:index]
+        get '/find_all', to: 'search#index'
         get '/find', to: 'search#show'
         get '/', to: 'merchants#index'
         post '/', to: 'merchants#create'
