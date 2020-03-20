fa6c40d9a14f63a742569ca8efb3c744d7c1ed16
David Atkins <atki1080@gmail.com>
2020-03-19 21:22:48 -0600

    Add items search index route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
       end
 
       namespace :items do
+        get '/find_all', to: 'search#index'
         get '/find', to: 'search#show'
         get '/', to: 'items#index'
         post '/', to: 'items#create'
