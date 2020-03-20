9ba943faaaff8da2b4a2c315c3917862b93927ee
David Atkins <atki1080@gmail.com>
2020-03-19 18:04:02 -0600

    Include searchable module

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
+require './lib/modules/searchable'
+
 class Item < ApplicationRecord
+  include Searchable
+
   validates_presence_of :name, :description, :unit_price
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
