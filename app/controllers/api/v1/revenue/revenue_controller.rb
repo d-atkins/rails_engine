6334ac1b755706da8587c8e40c481d7d80c10756
David Atkins <atki1080@gmail.com>
2020-03-21 21:17:27 -0600

    Round revenue_across to two decimal places

--- app/controllers/api/v1/revenue/revenue_controller.rb
+++ app/controllers/api/v1/revenue/revenue_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Revenue::RevenueController < ApplicationController
   def show
-    revenue = Merchant.revenue_across(params[:start], params[:end])
+    revenue = Merchant.revenue_across(params[:start], params[:end]).round(2)
     render json: {data: {'id' => nil, attributes: {revenue: revenue}}}
   end
 end
