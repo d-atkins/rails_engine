d21ddd2c0ea9a8275de9337693a425387dcb5e1e
David Atkins <atki1080@gmail.com>
2020-03-21 21:58:38 -0600

    Move all print logic into reset_all_tables

--- lib/helpers/importer.rb
+++ lib/helpers/importer.rb
@@ -3,38 +3,40 @@ require 'csv'
 class Importer
 
   def initialize
-    @models = [Customer, Merchant, Item, Invoice, InvoiceItem, Transaction]
+    @models = [Customer, Merchant, Invoice, Transaction, Item, InvoiceItem]
   end
 
-  def import(path, model, print)
-    print "Importing #{model} records... " if print
+  def import(path, model)
     csv_text = File.read("#{path}#{model.to_s.underscore}s.csv")
     csv = CSV.parse(csv_text, :headers => true)
     csv.each do |row|
       model.create(row.to_hash)
     end
-    puts "DONE" if print
   end
 
-  def destroy_all_records(print)
-    @models.reverse.each do |model|
-      print "Destroying #{model} records... " if print
+  def destroy_records(model)
       model.destroy_all
-      puts "DONE" if print
-    end
   end
 
-  def reset_pk_sequences(print)
+  def reset_pk_sequences
     ActiveRecord::Base.connection.tables.each do |t|
       ActiveRecord::Base.connection.reset_pk_sequence!(t)
     end
-    puts "PK sequences reset." if print
   end
 
   def reset_all_tables(path, print = false)
-    destroy_all_records(print)
-    @models.each { |model| import(path, model, print) }
-    reset_pk_sequences(print)
+    @models.reverse.each do |model|
+      print "Destroying #{model} records... " if print
+      destroy_records(model)
+      puts "DONE" if print
+    end
+    @models.each do |model|
+      print "Importing #{model} records... " if print
+      import(path, model)
+      puts "DONE" if print
+    end
+    reset_pk_sequences
+    puts "PK sequences reset." if print
     puts "Reseed complete!" if print
   end
 
