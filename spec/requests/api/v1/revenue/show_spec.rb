114b5fe38f3b8e7812244f2a0d5016d6f008663f
David Atkins <atki1080@gmail.com>
2020-03-21 19:28:09 -0600

    Add revenue show spec

---
+++ spec/requests/api/v1/revenue/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Revenue API:', type: :request do
+  before(:each) do
+    importer = Importer.new
+    importer.reset_all_tables('./spec/fixtures/truncated/')
+  end
+
+  it 'sends the revenue across all merchants between the given dates' do
+    get '/api/v1/revenue?start=2012-03-27&end=2012-03-30'
+
+    revenue = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(revenue['data']['id']).to eq(nil)
+    expect(revenue['data']['attributes']['revenue']).to eq(102058.34)
+  end
+
+end
