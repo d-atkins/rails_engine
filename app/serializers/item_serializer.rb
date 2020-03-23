3f64766be1cbcb18426d356d834b2dd6a73a5bad
David Atkins <atki1080@gmail.com>
2020-03-23 01:44:37 -0600

    Add merchant_name attribute

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,5 +2,9 @@ class ItemSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :description, :unit_price, :merchant_id
 
+  attribute :merchant_name do |object|
+    object.merchant.name
+  end
+
   belongs_to :merchant
 end
